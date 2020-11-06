629a281e3220ccc98aaefe6909828f4ddd668113
Oleg <kolyushko.oleg@gmail.com>
2020-11-06 20:26:20 +0200

    Updates

---
+++ precache-manifest.153a683a548f0addb3a134ed2911d0ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb800648b59a2e5a0395de235aa8649f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "02ade1c632f544c60d35",
+    "url": "/goit-react-hw-01-components/static/css/main.c308b3c1.chunk.css"
+  },
+  {
+    "revision": "e48e051d1dec912f07ce",
+    "url": "/goit-react-hw-01-components/static/js/2.4f636958.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.4f636958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ade1c632f544c60d35",
+    "url": "/goit-react-hw-01-components/static/js/main.16561639.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
